b897e64d584fd781ebcb83fc04261fdd0b2032cf
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-05-17 19:46:14 -0500

    Update urls.py

--- littlelemon/littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/littlelemon/urls.py
@@ -25,7 +25,8 @@ router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
+    path('api/bookings/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
+
